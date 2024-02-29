b8d19654c7c1af3aaed1970e0f388fbb1039eb1f
pitwegner <pit.wegner@student.hpi.de>
2024-02-29 15:10:50 +0100

    fix typo

--- src/evohelix_nats/client.py
+++ src/evohelix_nats/client.py
@@ -98,7 +98,7 @@ class NATSClient(object):
                 if js:
                     await msg.nak()
                 else:
-                    await self.reply(msg.reply, '{"error": "unauthorized}', status=401)
+                    await self.reply(msg.reply, '{"error": "unauthorized"}', status=401)
         if js:
             return await self.js.subscribe(subject, cb=auth_middleware)
         return await self.client.subscribe(subject, cb=auth_middleware)
